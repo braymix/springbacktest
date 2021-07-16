aa53e570332e6c96754aabb4627f56b5793bced1
MICHELE PANAROTTO <michele.panarotto@tecnicosuperiorekennedy.it>
2021-07-16 18:02:58 +0200

    sistemato

--- src/test/java/it/simulazione/secondaSimulazione/SecondaSimulazioneApplicationTests.java
+++ src/test/java/it/simulazione/secondaSimulazione/SecondaSimulazioneApplicationTests.java
@@ -1,13 +1,13 @@
-package it.simulazione.secondaSimulazione;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SecondaSimulazioneApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package it.simulazione.secondaSimulazione;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SecondaSimulazioneApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
